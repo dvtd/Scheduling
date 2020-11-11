8eed2733e75956edb23ae03c66eef6db13d82adb
toannknknk <nguyenkhanhtoan812@gmail.com>
2020-11-11 20:29:47 +0700

    add param for auth controller and fix httppost in empConstraintController

--- Scheduling.Data/Dtos/BaseDto.cs
+++ Scheduling.Data/Dtos/BaseDto.cs
@@ -6,8 +6,8 @@ namespace Scheduling.Data.Dtos
 {
    public class BaseDto
     {
-        public DateTime CreateTime { get; set; }
-        public DateTime UpdateTime { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public DateTime? UpdateTime { get; set; }
         public string CreatePerson { get; set; }
         public string UpdatePerson { get; set; }
 
